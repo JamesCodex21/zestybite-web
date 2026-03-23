d02c8620cd9722f68d7101fd729e7110d9cb31fe
JamesCodex21 <Jranzlybontuyan@gmail.com>
2026-03-23 08:43:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -32,22 +32,22 @@ app.get('/api/menu', (req, res) => {
       { name: 'Zesty Burger',        price: 20, emoji: '🍔', img: '' },
       { name: 'Large Pizza',         price: 40, emoji: '🍕', img: '' },
       { name: 'Buffalo Chicken',     price: 15, emoji: '🍗', img: '' },
-      { name: 'Loaded Nachos',       price: 15, emoji: '🌮', img: 'https://i.ibb.co/gZYwQqWM/Loaded-Nachos.png' },
-      { name: 'Grilled Cheese',      price: 15, emoji: '🧇', img: 'https://i.ibb.co/5gLjG41f/Grilled-Cheese-Tomato-Soup.webp' },
-      { name: 'Chicken Tenders',     price: 12, emoji: '🍗', img: 'https://i.ibb.co/MDhwJ7LN/Chicken-Tenders.png' },
-      { name: 'Steak Sandwich',      price: 18, emoji: '🥪', img: 'https://i.ibb.co/3mk6SDMg/Steak-Sandwich.png' },
+      { name: 'Loaded Nachos',       price: 15, emoji: '🌮', img: 'https://drive.google.com/thumbnail?id=12YQCgt0jNR6PrQ410Nrjk7MvSMHL41PG&sz=w400' },
+      { name: 'Grilled Cheese',      price: 15, emoji: '🧇', img: 'https://drive.google.com/thumbnail?id=1ky32Z_Kb7breksj3zXCUjo3msny9VZPB&sz=w400' },
+      { name: 'Chicken Tenders',     price: 12, emoji: '🍗', img: '' },
+      { name: 'Steak Sandwich',      price: 18, emoji: '🥪', img: 'https://drive.google.com/thumbnail?id=1drxYXf9szX79hfhOhrL5BaNxx9BsUrIt&sz=w400' },
       { name: 'Hot Dogs',            price: 12, emoji: '🌭', img: '' },
       { name: 'BBQ Pork Sandwich',   price: 14, emoji: '🥪', img: '' },
-      { name: 'Fish and Chips',      price: 13, emoji: '🐟', img: 'https://i.ibb.co/N62HN4Y0/Fish-and-Chips.png' },
-      { name: 'Spaghetti Carbonara', price: 30, emoji: '🍝', img: 'https://i.ibb.co/WNTnXSnK/Spaghetti-Carbonara.png' },
-      { name: 'Chicken Quesadillas', price: 20, emoji: '🌮', img: 'https://i.ibb.co/Fkh1rH9x/Chicken-Quesadillas.png' },
+      { name: 'Fish and Chips',      price: 13, emoji: '🐟', img: 'https://drive.google.com/thumbnail?id=1Ua-F9ZeHxpE4MEBF1ytjrmAuyZ_nTWk0&sz=w400' },
+      { name: 'Spaghetti Carbonara', price: 30, emoji: '🍝', img: '' },
+      { name: 'Chicken Quesadillas', price: 20, emoji: '🌮', img: '' },
       { name: 'Classic Beef Stew',   price: 28, emoji: '🍲', img: '' },
-      { name: 'Adobo',               price: 18, emoji: '🍲', img: 'https://i.ibb.co/4RGpDmhY/Adobos.png' },
+      { name: 'Adobo',               price: 18, emoji: '🍲', img: '' },
     ],
     drinks: [
-      { name: 'Coca Cola',    price: 5, emoji: '🥤', img: 'https://i.ibb.co/S4rvh4HS/Coca-Cola.jpg' },
-      { name: 'Sprite',       price: 5, emoji: '🥤', img: 'https://i.ibb.co/pvgzgNY8/Sprite.png' },
-      { name: 'Orange Juice', price: 7, emoji: '🍊', img: 'https://i.ibb.co/fzQSVgy5/Orange-Juice.png' },
+      { name: 'Coca Cola',    price: 5, emoji: '🥤', img: 'https://drive.google.com/thumbnail?id=1idfPcL5JGeD-OcXtGZFGWFWPBZj51Kx6&sz=w400' },
+      { name: 'Sprite',       price: 5, emoji: '🥤', img: 'https://drive.google.com/thumbnail?id=18BEXMA72PwWQMhZki1NU9fxjCcmCjYRD&sz=w400' },
+      { name: 'Orange Juice', price: 7, emoji: '🍊', img: 'https://drive.google.com/thumbnail?id=1QRvZ6VqOrgLVGoCVdME3lqdt4okLWqN1&sz=w400' },
       { name: 'Mango Juice',  price: 7, emoji: '🥭', img: '' },
       { name: 'Iced Tea',     price: 6, emoji: '🧋', img: '' },
       { name: 'Lemonade',     price: 6, emoji: '🍋', img: '' },
@@ -56,14 +56,14 @@ app.get('/api/menu', (req, res) => {
       { name: 'Coffee',       price: 8, emoji: '☕', img: '' },
     ],
     sides: [
-      { name: 'French Fries',  price: 8, emoji: '🍟', img: 'https://i.ibb.co/nsNdfgNW/French-Fries.png' },
-      { name: 'Onion Rings',   price: 9, emoji: '🧅', img: 'https://i.ibb.co/XkDfTMPT/Onion-Rings.png' },
+      { name: 'French Fries',  price: 8, emoji: '🍟', img: 'https://drive.google.com/thumbnail?id=15EVHZvZBPR-j-T2iVjZM0XvIYuDAwybQ&sz=w400' },
+      { name: 'Onion Rings',   price: 9, emoji: '🧅', img: 'https://drive.google.com/thumbnail?id=1a4cvIzy8D6hFSq59w8TThvadTDCYBOn2&sz=w400' },
       { name: 'Coleslaw',      price: 6, emoji: '🥗', img: '' },
       { name: 'Mashed Potato', price: 7, emoji: '🥔', img: '' },
       { name: 'Garlic Bread',  price: 5, emoji: '🥖', img: '' },
       { name: 'Corn on Cob',   price: 6, emoji: '🌽', img: '' },
       { name: 'Side Salad',    price: 7, emoji: '🥗', img: '' },
-      { name: 'Mac and Cheese',price: 9, emoji: '🧀', img: 'https://i.ibb.co/gHmNNTc/mac-and-cheese.png' },
+      { name: 'Mac and Cheese',price: 9, emoji: '🧀', img: 'https://drive.google.com/thumbnail?id=1hGHQWnigP41m1fIRL2EEcBPdYba9JGCy&sz=w400' },
       { name: 'Steamed Rice',  price: 4, emoji: '🍚', img: '' },
     ]
   });
@@ -71,4 +71,3 @@ app.get('/api/menu', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`ZestyBite web server running on port ${PORT}`));
-
